6b08c579cf8bee6d95e2ce45da0a8630de8845bc
buecherregale <david.grimmen@gmail.com>
2026-01-03 13:35:07 +0100

    wip: export shell theme & create sample theme via online builder

--- composeApp/src/jvmMain/kotlin/dev/buecherregale/ebook_reader/main.kt
+++ composeApp/src/jvmMain/kotlin/dev/buecherregale/ebook_reader/main.kt
@@ -1,12 +1,27 @@
 package dev.buecherregale.ebook_reader
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.decodeToImageBitmap
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import com.materialkolor.ktx.themeColors
+import com.materialkolor.ktx.toHex
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlin.uuid.ExperimentalUuidApi
 
+fun calculateSeedColor(bitmap: ImageBitmap): Color {
+    val suitableColors = bitmap.themeColors(fallback = Color.Blue)
+    suitableColors.forEach { color -> println(color.toHex()) }
+    return suitableColors.first()
+}
+
 @OptIn(DelicateCoroutinesApi::class, ExperimentalUuidApi::class)
 fun main() = application {
+    java.nio.file.Path.of("/home/david/Pictures/Wallpapers/miku-redhair.jpg")
+        .toFile().inputStream().use {
+            calculateSeedColor(it.readAllBytes().decodeToImageBitmap())
+        }
     Window(
         onCloseRequest = ::exitApplication,
         title = "ebook_reader",
