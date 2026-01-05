b8f810be79efad96f76e010e45b879a339bd2186
buecherregale <david.grimmen@gmail.com>
2026-01-05 11:25:31 +0100

    fix: jvm: slightly better picker title

--- composeApp/src/jvmMain/kotlin/dev/buecherregale/ebook_reader/ui/FilePicker.kt
+++ composeApp/src/jvmMain/kotlin/dev/buecherregale/ebook_reader/ui/FilePicker.kt
@@ -4,9 +4,9 @@ import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.filechooser.FileNameExtensionFilter
 
-suspend fun <T> pickFile(mainType: String, types: List<String>, resultMapper: (File) -> T) : T? {
+fun <T> pickFile(mainType: String, types: List<String>, resultMapper: (File) -> T) : T? {
     val chooser = JFileChooser().apply {
-        dialogTitle = "Select an image"
+        dialogTitle = "Select $mainType"
         fileFilter = FileNameExtensionFilter(mainType, *types.toTypedArray())
     }
 
