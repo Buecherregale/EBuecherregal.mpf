63cff777572844205d6f2cc4745f3aa0e19976f6
buecherregale <david.grimmen@gmail.com>
2026-01-26 15:30:11 +0100

    restructure: create `desktopApp` module (similar to `android`)

---
+++ desktopApp/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    alias(libs.plugins.kotlinJvm)
+    alias(libs.plugins.composeMultiplatform)
+    alias(libs.plugins.composeCompiler)
+}
+
+kotlin {
+    dependencies {
+        implementation(projects.composeApp)
+
+        implementation(compose.desktop.currentOs)
+        implementation(libs.compose.ui.tooling.preview)
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "dev.buecherregale.ebook_reader.MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "dev.buecherregale.ebook_reader"
+            packageVersion = "1.0.0"
+        }
+    }
+}
